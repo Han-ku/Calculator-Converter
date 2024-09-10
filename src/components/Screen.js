440e8f376bd691f3402405c9ea6f822055890dc9
Hanna Kunitskaya <hanna_kunitskaya@gmail.com>
2024-09-10 18:33:54 +0200

    delete react-textfit

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,15 +1,14 @@
 import React, { useContext } from 'react'
 import { CalcContext } from '../context/CalcContext'
-import { Textfit } from 'react-textfit';
 
 const Screen = () => {
 
     const {calc} = useContext(CalcContext)
 
   return (
-    <Textfit className='screen' max={70} mode='single'>
+    <div className="screen">
       {calc.expression ? calc.expression : "0"}
-    </Textfit>
+    </div>
   )
 }
 
